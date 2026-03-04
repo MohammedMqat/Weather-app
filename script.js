025326a520bb038cb6f99fa685e34bb19a42694f
MohammedMqat <mohammedmqat@gmail.com>
2026-03-04 21:39:54 +0200

    graped Elements By Id

---
+++ script.js
@@ -0,0 +1,16 @@
+const apiKey = "";
+
+const weatherImage = document.getElementById("weatherImage");
+const weatherContainer = document.getElementById("weather-container");
+const searchContainer = document.getElementById("search-container");
+const searchInput = document.getElementById("search-input");
+const weatherInfo = document.getElementById("weather-info");
+const location = document.getElementById("location");
+const cityName = document.getElementById("city-name");
+const temperatureContainer = document.getElementById("temperature-container");
+const temperature = document.getElementById("temperature");
+const weatherIconContainer = document.getElementById("weather-icon-container");
+const weatherIcon = document.getElementById("weather-icon");
+const timeContainer = document.getElementById("time-container");
+const time = document.getElementById("time");
+const date = document.getElementById("date");
